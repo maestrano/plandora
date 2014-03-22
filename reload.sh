8bcb35ee932e25cca1aaa357a121a88e06ca339a
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-03-22 16:46:22 +1100

    guarding in case of failure

--- reload.sh
+++ reload.sh
@@ -4,8 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 ant war -Dmy_source=$DIR -Dtomcat_home=/usr/local/Cellar/tomcat6/6.0.39/libexec
+RETVAL=$?
 
-
-/usr/local/Cellar/tomcat6/6.0.39/libexec/bin/shutdown.sh
-sleep 3
-/usr/local/Cellar/tomcat6/6.0.39/libexec/bin/startup.sh
+if [ $RETVAL -eq 0 ]; then
+  /usr/local/Cellar/tomcat6/6.0.39/libexec/bin/shutdown.sh
+  sleep 3
+  /usr/local/Cellar/tomcat6/6.0.39/libexec/bin/startup.sh
+else
+  echo "Project compilation failed. Not proceeding with tomcat restart"
+fi
