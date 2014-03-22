f8982d3fe0fad245dd728d2dc0186d97dbf0c079
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-03-22 12:08:22 +1100

    fixed working dir

--- reload.sh
+++ reload.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-ant war -Dmy_source=/Users/arnaudlachaume/Sites/apps-dev/2_MNO_INTEGRATING/plandora/ -Dtomcat_home=/usr/local/Cellar/tomcat6/6.0.39/libexec
 
-cp -p context.xml /usr/local/Cellar/tomcat6/6.0.39/libexec/webapps/pandora/META-INF/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+ant war -Dmy_source=$DIR -Dtomcat_home=/usr/local/Cellar/tomcat6/6.0.39/libexec
+
 
 /usr/local/Cellar/tomcat6/6.0.39/libexec/bin/shutdown.sh
 sleep 3
